d2e07599c12b6681cb8350b843b804a5419ce539
= <sean.fioritto@gmail.com>
2014-04-24 14:02:44 -0500

    navigation

---
+++ present.js
@@ -0,0 +1,33 @@
+(function(){
+
+    var slides = document.getElementsByClassName("slide"),
+    active = slides[0],
+
+    nextSlide = function(){
+        if (active && active.nextElementSibling) {
+            active.classList.remove("active");
+            active = active.nextElementSibling;
+            active.classList.add("active");
+        }
+    },
+
+    prevSlide = function(){
+        if (active && active.previousElementSibling) {
+            active.classList.remove("active");
+            active = active.previousElementSibling;
+            active.classList.add("active");
+        }
+    };
+    
+    document.onmousedown = function(event) {
+        nextSlide();
+    }
+    
+    document.onkeyup = function(event) {
+        if (event.keyCode === 37) {
+            prevSlide();
+        } else if(event.keyCode === 39) { 
+            nextSlide();
+        }
+    }
+})();
